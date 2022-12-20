f5557d0157829a4f39bb44ac140c8b2ae9442a4b
Travis Kohlbeck <me+git@travisk.info>
2022-12-20 03:21:26 +0000

    Add tag component

---
+++ components/tag.js
@@ -0,0 +1,54 @@
+import styled from 'styled-components'
+import { getContrastColor } from '../color'
+
+const Container = styled.span`
+	display: inline-flex;
+	position: relative;
+	border-radius: 5px;
+	overflow: hidden;
+	cursor: pointer;
+	white-space: nowrap;
+	font-family: Nunito, Helvetica, sans-serif;
+	height: 30px;
+	user-select: none;
+`
+
+const Name = styled.span`
+	display: inline-flex;
+	position: relative;
+	align-items: center;
+	justify-content: center;
+	background-color: ${p => p.color};
+	color: ${p => p.contrastColor};
+	font-weight: 700;
+	height: 100%;
+	padding-left: 8px;
+	padding-right: 6px; /* -2 from value border */
+`
+
+const Value = styled.span`
+	display: inline-flex;
+	position: relative;
+	align-items: center;
+	justify-content: center;
+	border: 2px solid ${p => p.color};
+	color: ${p => p.color};
+	background-color: ${p => p.contrastColor};
+	border-radius: 5px;
+	border-bottom-left-radius: 0;
+	border-top-left-radius: 0;
+	font-weight: 700;
+	height: 100%;
+	padding: 0 6px;
+`
+
+export default function Tag({ name, value, color, onClick }) {
+	const contrastColor = 'white'
+	const props = { color, contrastColor }
+	return (
+		<Container onClick={onClick}>
+			<Name {...props}>{name}</Name>
+			{value && <Value {...props}>{value}</Value>}
+		</Container>
+	)
+}
