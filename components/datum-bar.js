26653d75dff68048885d185c7e2c76143e9df764
Travis Kohlbeck <me+git@travisk.info>
2022-12-23 13:53:00 -0500

    Add NewTagButton, begin work on NewTagInput

--- components/datum-bar.js
+++ components/datum-bar.js
@@ -11,6 +11,52 @@ const InputBar = styled(Box)`
 	justify-content: space-between;
 `
 
+const TagInputButton = styled.button`
+	display: inline-flex;
+	position: relative;
+	align-items: center;
+	justify-content: center;
+	border: 1px solid grey;
+	height: 30px;
+	border-radius: 5px;
+	margin-left: 10px;
+	color: grey;
+	font-family: Nunito;
+	font-weight: 700;
+	font-size: 14px;
+	padding-right: 10px;
+	padding-left: 5px;
+	white-space: nowrap;
+	&:hover {
+		border-color: white;
+		color: white;
+	}
+`
+
+const TagInputButtonIcon = styled(AddDatumButtonIcon)`
+	/* padding: 10px; */
+	width: 20px;
+	height: 20px;
+	margin-right: 5px;
+`
+
+const TagInput = styled.input`
+	border: 1px solid grey;
+	height: 30px;
+	border-radius: 5px;
+	margin-left: 10px;
+	color: white;
+	font-family: Nunito;
+	font-weight: 700;
+	font-size: 16px;
+	&:hover {
+		border-color: white;
+	}
+	&:active {
+		border-color: white;
+	}
+`
+
 const AddDatumButton = styled.button`
 	display: inline-flex;
 	align-items: center;
@@ -23,6 +69,10 @@ const AddDatumButton = styled.button`
 export default function DatumBar() {
 	return (
 		<InputBar>
+			<TagInputButton>
+				<TagInputButtonIcon />
+				New tag
+			</TagInputButton>
 			<AddDatumButton>
 				<AddDatumButtonIcon />
 			</AddDatumButton>
