6c10e5c66ad920fe7ab791e026d68c9b3bfb4482
Travis Kohlbeck <me+git@travisk.info>
2022-12-23 06:02:15 -0500

    Attempt to fix PWA font rendering

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,22 @@
+import { Nunito } from '@next/font/google'
+
 import '../styles/globals.css'
 
+const nunito = Nunito({
+	weight: ['400', '700'],
+	subsets: ['latin'],
+	display: 'swap',
+})
+
 export default function App({ Component, pageProps }) {
-	return <Component {...pageProps} />
+	return (
+		<>
+			<style jsx global>{`
+				html {
+					font-family: ${nunito.style.fontFamily};
+				}
+			`}</style>
+			<Component {...pageProps} />
+		</>
+	)
 }
