9f1f61f1f488871a8ebdfbcbcae2dd6605d4261a
Travis Kohlbeck <me@travisk.dev>
2024-06-10 02:06:52 -0400

    Add new tags to the active datum

--- app/page.tsx
+++ app/page.tsx
@@ -31,10 +31,12 @@ export function Button({ children }: { children: React.ReactNode }) {
 
 export function Tag({ name, value, unit, color }: Tag) {
   let roundedValue = 'rounded-tr rounded-br'
+  let padding = 'pr-[6px]'
   if (unit) roundedValue = ''
+  if (!value) padding = 'pr-[8px]'
   return (
     <span className='inline-flex rounded overflow-hidden h-[30px] font-bold mr-[5px] whitespace-nowrap'>
-      <button className={`inline-flex relative items-center pr-[6px] pl-[8px]`} style={{ backgroundColor: color, color: getContrastColor(color) }}>{name}</button>
+      <button className={`inline-flex relative items-center ${padding} pl-[8px]`} style={{ backgroundColor: color, color: getContrastColor(color) }}>{name}</button>
       {value && <button className={`inline-flex relative items-center px-[6px] border-2 ${roundedValue}`} style={{ color, borderColor: color, background: getContrastColor(color) }}>{value}</button>}
       {unit && <button className='pr-[6px] pl-[4px]' style={{ background: color, color: getContrastColor(color) }}>{unit}</button>}
     </span>
@@ -45,7 +47,7 @@ export function Datum({ tags }: { tags: Tag[] }) {
   return (
     <li className='flex relative items-center justify-between mx-[10px] border-b border-neutral-700 w-100%'>
       <span className='container inline-flex relative overflow-auto'>
-        <span className='sub-container inline-flex relative grow justify-start overflow-auto'>
+        <span className='datum-tag-sub-container inline-flex relative grow justify-start overflow-auto'>
           <span className='content inline-flex relative gap-[5px] py-[10px] pr-[20px]'>
             {tags.map((tag, i) => <Tag key={i} {...tag} />)}
           </span>
@@ -60,10 +62,16 @@ export function Datum({ tags }: { tags: Tag[] }) {
   )
 }
 
-export function TagNameMenu() {
+export function TagNameMenu({ isVisible }: { isVisible: boolean }) {
+  let height = 'h-0 opacity-0'
+  let border = 'border-b-0'
+  if (isVisible) {
+    height = 'h-[50px] opacity-100'
+    border = 'border-b-[1px]'
+  }
   return (
-    <div className='px-[10px] w-full'>
-      <div className='flex justify-start w-full py-[10px] border-b border-neutral-700'>
+    <div className={`tag-name-menu px-[10px] w-full ${height}`}>
+      <div className={`flex justify-start w-full pt-[10px] border-neutral-700`}>
         <Tag {...{ name: 'test', color: getRandomHex(6) }} />
       </div>
     </div>
@@ -72,9 +80,10 @@ export function TagNameMenu() {
 
 export default function App() {
   const [datums, setDatums] = useState([])
-  const [activeTags, setActiveTags] = useState([])
+  const [activeTags, setActiveTags] = useState<Tag[]>([])
   const [isTagNameMenuVisible, setIsTagNameMenuVisible] = useState(false)
   const [isNewTagBtnAnInput, setIsNewTagBtnAnInput] = useState(false)
+  const [newTagNameInputValue, setNewTagNameInputValue] = useState('')
 
   useEffect(() => {
     fetch('http://localhost:3000/api/datums')
@@ -82,18 +91,32 @@ export default function App() {
       .then(json => setDatums(json))
   }, [])
 
-  function beginCreateActiveTag(e) {
+  function beginCreateActiveTag() {
     setIsTagNameMenuVisible(true)
     setIsNewTagBtnAnInput(true)
   }
 
-  function endCreateActiveTag(e) {
+  function endCreateActiveTag(e: { target: { value: any; }; }) {
     if (e.target.value) return
     setIsTagNameMenuVisible(false)
     setIsNewTagBtnAnInput(false)
   }
 
+  function createTagName(e) {
+    const tagName = newTagNameInputValue
+    // check if it exists already
+    // if not, add to tag name menu
 
+    setActiveTags([
+      ...activeTags,
+      {
+        name: tagName,
+        color: getRandomHex(6),
+      }
+    ])
+    setNewTagNameInputValue('')
+    endCreateActiveTag(e)
+  }
 
   return (
     <main className="flex flex-col w-full h-full items-center justify-between font-nunito text-sm ">
@@ -115,12 +138,25 @@ export default function App() {
         </ul>
       </section>
       <footer className='flex flex-col relative items-center justify-between bottom-0 h-auto w-full border-t border-neutral-700 bg-black'>
-        {isTagNameMenuVisible && <TagNameMenu />}
-        <div className='flex relative items-center justify-between w-full'>
-          {isNewTagBtnAnInput
-            ? <input autoFocus className='new-tag-input border rounded border-neutral-700 w-[80px] h-[30px] ml-2 px-2 bg-black focus:border-white' onBlur={endCreateActiveTag}></input>
-            : <button className='border rounded border-neutral-700 w-[80px] h-[30px] ml-2 px-2' onClick={beginCreateActiveTag}>New tag</button>
-          }
+        <TagNameMenu isVisible={isTagNameMenuVisible} />
+        <div className='active-datum flex relative items-center justify-between w-full pl-2'>
+          <div className='flex'>{activeTags.map(tag => <Tag {...tag} />)}
+            {isNewTagBtnAnInput
+              ? <span className='flex items-center justify-center'>
+                <input
+                  className='new-tag-input border rounded border-neutral-700 w-[80px] h-[30px] px-2 bg-black focus:border-white'
+                  autoFocus
+                  value={newTagNameInputValue}
+                  onBlur={endCreateActiveTag}
+                  onChange={(e) => setNewTagNameInputValue(e.target.value)}
+                ></input>
+                {newTagNameInputValue && <button className='flex justify-center w-[25px] text-lg' onClick={createTagName}><FaPlus /></button>}
+              </span>
+              : <button
+                className='border rounded border-neutral-700 w-[80px] h-[30px] px-2'
+                onClick={beginCreateActiveTag}
+              >New tag</button>
+            }</div>
           <button className='flex items-center justify-center text-3xl w-[50px] h-[50px] text-neutral-500 active:hover:text-white'><FaPlus /></button>
         </div>
       </footer>
